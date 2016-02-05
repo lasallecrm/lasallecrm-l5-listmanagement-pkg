bbcafafab659305b0b675be992eba7f821943c22
Bob Bloom <info@southlasalle.com>
2016-02-05 09:14:54 -0500

    Update adamwathan.php with nitpick-ci.com
    Update adamwathan.php with nitpick-ci.com code review suggestions.

--- src/adamwathan.php
+++ src/adamwathan.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Lasallecrm\Listmanagement;
+
 // this file created in order to try nitpick-ci.com.
 
 // https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-2-coding-style-guide.md
@@ -8,20 +10,17 @@ use FooInterface;
 use BarClass as Bar;
 use OtherVendor\OtherPackage\BazClass;
 
-class adamwathan extends Bar implements FooInterface {
+class AdamWathan extends Bar implements FooInterface 
+{
     public function sampleFunction($a, $b = null) {
-        if ($a === $b) 
-        {
+        if ($a === $b) { 
             bar();
         } 
-        elseif ($a > $b) 
-        {
-                  $foo->bar($arg1);
-        } 
-          else 
-          {
+        elseif ($a > $b) { 
+            $foo->bar($arg1);
+        } else { 
             BazClass::bar($arg2, $arg3);
-        }
+        } 
     }
 
     final public static function bar() {
@@ -29,4 +28,4 @@ class adamwathan extends Bar implements FooInterface {
     }
 }
 
-?>
+
