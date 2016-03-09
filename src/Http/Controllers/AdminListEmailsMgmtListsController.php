e8e315b05ca7a068e4ac34f8714cb02416153d14
Bob Bloom <info@southlasalle.com>
2016-03-09 15:09:52 -0500

    Fix some PSR-2 coding formatting. #13

--- src/Http/Controllers/AdminListEmailsMgmtListsController.php
+++ src/Http/Controllers/AdminListEmailsMgmtListsController.php
@@ -1,36 +1,37 @@
 <?php
+
 namespace Lasallecrm\Listmanagement\Http\Controllers;
 
-    /**
-     *
-     * To Do package for the LaSalle Customer Relationship Management package.
-     *
-     * Based on the Laravel 5 Framework.
-     *
-     * Copyright (C) 2015 - 2016  The South LaSalle Trading Corporation
-     *
-     * This program is free software: you can redistribute it and/or modify
-     * it under the terms of the GNU General Public License as published by
-     * the Free Software Foundation, either version 3 of the License, or
-     * (at your option) any later version.
-     *
-     * This program is distributed in the hope that it will be useful,
-     * but WITHOUT ANY WARRANTY; without even the implied warranty of
-     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-     * GNU General Public License for more details.
-     *
-     * You should have received a copy of the GNU General Public License
-     * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-     *
-     *
-     * @package    To Do package for the LaSalle Customer Relationship Management package
-     * @link       http://LaSalleCRM.com
-     * @copyright  (c) 2015 - 2016, The South LaSalle Trading Corporation
-     * @license    http://www.gnu.org/licenses/gpl-3.0.html
-     * @author     The South LaSalle Trading Corporation
-     * @email      info@southlasalle.com
-     *
-     */
+/**
+ *
+ * To Do package for the LaSalle Customer Relationship Management package.
+ *
+ * Based on the Laravel 5 Framework.
+ *
+ * Copyright (C) 2015 - 2016  The South LaSalle Trading Corporation
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * @package    To Do package for the LaSalle Customer Relationship Management package
+ * @link       http://LaSalleCRM.com
+ * @copyright  (c) 2015 - 2016, The South LaSalle Trading Corporation
+ * @license    http://www.gnu.org/licenses/gpl-3.0.html
+ * @author     The South LaSalle Trading Corporation
+ * @email      info@southlasalle.com
+ *
+ */
 
 // LaSalle Software
 use Lasallecms\Formhandling\AdminFormhandling\AdminFormBaseController;
@@ -45,18 +46,17 @@ use Lasallecms\Lasallecmsapi\Repositories\BaseRepository;
 use Lasallecrm\Listmanagement\Models\List_Email as Model;
 
 
-/*
+/**
  * Resource controller for administration of posts
  */
 class AdminListEmailsMgmtListsController extends AdminFormBaseController
 {
-    /*
+    /**
      * @param  Model, as specified above
      * @param  Lasallecms\Lasallecmsapi\Repositories\BaseRepository
      * @return void
      */
-    public function __construct(Model $model, BaseRepository $repository)
-    {
+    public function __construct(Model $model, BaseRepository $repository) {
         // execute AdminController's construct method first in order to run the middleware
         parent::__construct();
 
--- src/Http/Controllers/AdminListMgmtListsController.php
+++ src/Http/Controllers/AdminListMgmtListsController.php
@@ -46,18 +46,17 @@ use Lasallecms\Lasallecmsapi\Repositories\BaseRepository;
 use Lasallecrm\Listmanagement\Models\Listlist as Model;
 
 
-/*
+/**
  * Resource controller for administration of posts
  */
 class AdminListMgmtListsController extends AdminFormBaseController
 {
-    /*
+    /**
      * @param  Model, as specified above
      * @param  Lasallecms\Lasallecmsapi\Repositories\BaseRepository
      * @return void
      */
-    public function __construct(Model $model, BaseRepository $repository)
-    {
+    public function __construct(Model $model, BaseRepository $repository) {
         // execute AdminController's construct method first in order to run the middleware
         parent::__construct();
 
